ca3e2ea01e5500f5f467f12a36a99214050673d9
Melissa <melissa.4.kennedy@gmail.com>
2020-05-13 15:55:35 -0400

    Add unique constraint on ingreident model

--- pantry/core/models.py
+++ pantry/core/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class Ingredient(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
 
     objects = models.Manager()
