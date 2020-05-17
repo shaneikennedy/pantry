4eb55125edb935ecdb054ec23c58e36e4e687f05
Shane Kennedy <shane.kennedy19@gmail.com>
2020-05-17 20:19:57 +0200

    Create basic ingredients list endpoint

---
+++ pantry/core/serializers.py
@@ -0,0 +1,10 @@
+from .models import Ingredient
+
+from rest_framework import serializers
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Ingredient
+        fields = ('id', 'name')
---
+++ pantry/core/views.py
@@ -0,0 +1,14 @@
+from .serializers import IngredientSerializer
+from .models import Ingredient
+from rest_framework.views import APIView, Response
+
+
+class IngredientAPIView(APIView):
+
+    def get(self, request):
+        ingredients = Ingredient.objects.all()
+        serializer = IngredientSerializer(ingredients, many=True)
+        return Response(serializer.data)
+
+
+ingredients_api = IngredientAPIView.as_view()
