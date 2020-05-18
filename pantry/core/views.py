026885e1cf4009cf8cf2fb9163505f9ed836e93c
Melissa <melissa.4.kennedy@gmail.com>
2020-05-18 11:19:22 -0400

    Implement search on ingredients API

--- pantry/core/views.py
+++ pantry/core/views.py
@@ -7,7 +7,13 @@ class IngredientAPIView(APIView):
 
     def get(self, request):
         ingredients = Ingredient.objects.all()
+        item = self.request.query_params.get("item", "")
+
+        if item != "":
+            ingredients = ingredients.filter(name=item)
+
         serializer = IngredientSerializer(ingredients, many=True)
+
         return Response(serializer.data)
 
 
