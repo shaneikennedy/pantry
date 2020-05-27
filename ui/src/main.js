fa48eee31bc41dfbded680de4ddd89e17abb37d4
Shane Kennedy <shane.kennedy19@gmail.com>
2020-05-27 21:32:50 +0200

    Set up vue router

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,8 +1,32 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App.vue';
+
+
+import './styles/colors.css';
+
+
+const routes = [
+  {
+    path: '/',
+    component: Home,
+    name: 'home',
+  },
+  {
+    path: '/recipes',
+    component: Recipes,
+    name: 'recipes',
+  },
+];
+
+Vue.use(VueRouter);
+const router = new VueRouter({
+  routes,
+});
 
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app');
