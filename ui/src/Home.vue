c7d7e8c7dfb49ae2a75bf114e7e4a7c5d96467c4
Melissa <melissa.4.kennedy@gmail.com>
2020-06-17 16:15:16 -0400

    Create landing page

--- ui/src/Home.vue
+++ ui/src/Home.vue
@@ -1,3 +1,27 @@
 <template>
-  <p class="text-6xl font-thin">welcome</p>
+  <div class="TagLine BackgroundImage pt-48">
+    <h1 class="text-6xl text-center pantry-text-brown mb-8">
+      Be your own personal chef
+    </h1>
+    <router-link
+      class="flex items-center button-center pantry-bg-yellow pantry-text-brown font-bold text-2xl py-4 px-8 rounded focus:outline-none focus:shadow-outline mx-auto"
+      :to="{ name: 'register' }"
+      tag="button"
+    >
+      Get Started
+    </router-link>
+  </div>
 </template>
+
+<style scoped>
+.TagLine {
+  font-family: "Rubik", sans-serif;
+}
+
+.BackgroundImage {
+  background-image: url(assets/pantry-background-2.png);
+  background-position: top;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
