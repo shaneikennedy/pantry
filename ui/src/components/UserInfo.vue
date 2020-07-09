260946746a0ce2adfa7372432fa562ace514596c
Melissa <melissa.4.kennedy@gmail.com>
2020-07-09 13:34:40 -0400

    Display user recipes to profile page

---
+++ ui/src/components/UserInfo.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="max-w-full mx-auto rounded overflow-hidden shadow-lg">
+    <div class="pantry-bg-yellow2 p-4">
+      <div class="h-16 w-16 bg-gray-400 rounded-full mx-auto">
+        <p class="text-center text-white text-3xl">{{ profile.username[0] }}</p>
+      </div>
+      <span class="block text-gray-700 text-center px-4 py-2 text-xl">
+        {{ profile.username }}
+        <br />
+      </span>
+      <span class="block text-gray-700 text-center px-4 py-2 text-sm">
+        User Since:
+        {{ translateDate(profile.date_joined) }}
+        <br />
+      </span>
+      <span class="block text-gray-700 text-center px-4 py-2 text-sm">
+        Email: {{ profile.email }}
+        <br />
+      </span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "UserInfo",
+  props: {
+    profile: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    translateDate(date) {
+      return new Date(date).toDateString();
+    }
+  }
+};
+</script>
