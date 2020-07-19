0b6a5dbf1068bbe34b30f1730953eea25b2eb9e0
Melissa <melissa.4.kennedy@gmail.com>
2020-07-19 15:10:44 -0400

    Clear user profile on log out

--- ui/src/components/Navbar.vue
+++ ui/src/components/Navbar.vue
@@ -7,11 +7,7 @@
       <button
         class="flex items-center px-3 py-2 border rounded border-teal-400 hover:text-white hover:border-white"
       >
-        <svg
-          class="fill-current h-3 w-3"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
+        <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
           <title>Menu</title>
           <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
         </svg>
@@ -23,30 +19,17 @@
           :to="{ name: 'recipes' }"
           class="block mt-4 lg:inline-block text-white lg:mt-0 hover:text-white mr-4"
           tag="button"
-        >
-          Recipes
-        </router-link>
+        >Recipes</router-link>
       </div>
     </div>
     <router-link
-      v-show="profile"
+      v-if="profile"
       :to="{ name: 'profile' }"
       class="block mt-4 lg:inline-block text-white lg:mt-0 hover:text-white mr-4"
       tag="button"
-    >
-      {{ profile.username }}
-    </router-link>
-    <button v-show="user" class="text-white" @click="logout">
-      Logout
-    </button>
-    <router-link
-      v-show="isLoggedOut"
-      class="text-white"
-      tag="button"
-      :to="{ name: 'login' }"
-    >
-      Login
-    </router-link>
+    >{{ profile.username }}</router-link>
+    <button v-show="user" class="text-white" @click="logout">Logout</button>
+    <router-link v-show="isLoggedOut" class="text-white" tag="button" :to="{ name: 'login' }">Login</router-link>
   </nav>
 </template>
 
