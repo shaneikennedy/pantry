d39a946f9ae21bd5e2c142f5e3b6903e5b58ae78
Shane Kennedy <shane.kennedy19@gmail.com>
2020-12-29 13:46:52 -0500

    Use DATABASE_URL when available

--- pantry/settings.py
+++ pantry/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+import dj_database_url
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -85,16 +86,21 @@ WSGI_APPLICATION = 'pantry.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'pantry',
-        'USER': 'postgres',
-        'PASSWORD': 'root',
-        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
-        'PORT': '5432',
+
+
+if os.getenv('DATABASE_URL'):
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'pantry',
+            'USER': 'postgres',
+            'PASSWORD': 'root',
+            'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+            'PORT': '5432',
+        }
     }
-}
 
 
 # Password validation
